fe18c977281fe70194de1db84cceab9b27ce0693
anik581 <581anik@gmail.com>
2014-07-06 22:12:58 +0200

    Authorizatin for edit and update action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in user 				 #user take new remember_token
-			redirect_to current_user
+			sign_in user 				 		#user take new remember_token
+			redirect_back_or user 	#redirect_to current_user
 		else
 			flash.now[:error] = "Invalid email/password combination"
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
   	@user = User.new
@@ -20,11 +22,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #this line is defined in correct_user
   end
 
   def update
-    @user = User.find_by(params[:id])
+    #@user = User.find(params[:id]) #this line is defined in correct_user
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -36,8 +38,21 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user #construction ':notice' doesn’t work for the :error or :success keys.
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please Sign in."
+                        #flash[:notice] = "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end    
 
 end
